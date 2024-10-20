1643ca9049ce044cbd7299f268f99c29fed95e82
FranChesK0 <khl_doss@mail.ru>
2024-10-20 16:53:12 +0800

    add msg functions

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+function msg_info() {
+    echo -e "${green}[INFO] $* ${plain}"
+}
+
+function msg_warn() {
+    echo -e "${yellow}[WARN] $* ${plain}"
+}
+
+function msg_error() {
+    echo -e "${red}[ERROR] $* ${plain}"
+}
